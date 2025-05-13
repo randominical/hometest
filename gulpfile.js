e944425376aacd8b7159dd43d463733954d82546
randominical <aa.agafonova@gmail.com>
2025-05-13 21:42:03 +0400

    custom theme structure & gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,34 @@
 const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const concat = require("gulp-concat");
-const jsAllPath = "./wp-content/themes/hometest/assets/src/js/*.js";
-const scssAllPath = "./wp-content/themes/hometest/assets/src/sass/*.scss";
-const scssPath = "./wp-content/themes/hometest/assets/src/sass/style.scss";
+const uglify = require("gulp-uglify");
+const cleanCSS = require("gulp-clean-css");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const jsAllPath = "./wp-content/themes/randominical-theme/assets/src/js/*.js";
+const scssAllPath = "./wp-content/themes/randominical-theme/assets/src/sass/*.scss";
+const scssPath = "./wp-content/themes/randominical-theme/assets/src/sass/style.scss";
+
 
 function js() {
   return src(jsAllPath)
     .pipe(concat("script.js"))
-    .pipe(dest("./wp-content/themes/hometest/assets/js/"));
+    .pipe(uglify())
+    .pipe(dest("./wp-content/themes/randominical-theme/assets/js/"));
 }
 
 function css() {
   return src(scssPath)
     .pipe(sass().on("error", sass.logError))
-    .pipe(dest("./wp-content/themes/hometest/assets/css/"));
+    .pipe(postcss([autoprefixer()]))
+    .pipe(cleanCSS({ compatibility: "ie11" }))
+    .pipe(dest("./wp-content/themes/randominical-theme/assets/css/"));
 }
 
 exports.watch = function () {
   watch(scssAllPath, css);
   watch(jsAllPath, js);
 };
+
+exports.css = css;
+exports.js = js;
