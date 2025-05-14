aa9f284552b197f717f6ca3ce27ef0811acc308c
randominical <aa.agafonova@gmail.com>
2025-05-14 21:19:05 +0400

    single.php fix, basic functionality & readme

--- wp-content/plugins/my-plugin/includes/mfp-functions.php
+++ wp-content/plugins/my-plugin/includes/mfp-functions.php
@@ -24,3 +24,22 @@ function mfp_Add_My_Admin_Link()
 function mfp_render_admin_page() {
     include plugin_dir_path(__FILE__) . '/mfp-first-acp-page.php';
 }
+
+// Plugin function
+function wpb_follow_us($content) {
+ 
+// Only do this when a single post is displayed
+if ( is_single() ) { 
+ 
+// Message you want to display after the post
+// Add URLs to your own Twitter and Facebook profiles
+ 
+$content .= '<p class="follow-us">If you liked this article, then please follow us on <a href="http://twitter.com/wpbeginner" title="WPBeginner on Twitter" target="_blank" rel="nofollow">Twitter</a> and <a href="https://www.facebook.com/wpbeginner" title="WPBeginner on Facebook" target="_blank" rel="nofollow">Facebook</a>.</p>';
+ 
+}
+// Return the content
+return $content; 
+ 
+}
+// Hook our function to WordPress the_content filter
+add_filter('the_content', 'wpb_follow_us'); 
